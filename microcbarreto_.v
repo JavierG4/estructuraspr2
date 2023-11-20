1e349fa28a1aa6c882743870fc651cb5e8688abd
alu0101552774 <alu0101552774@ull.edu.es>
2023-11-20 21:23:30 +0100

    a

--- microcbarreto_.v
+++ microcbarreto_.v
@@ -1,90 +1,94 @@
-module unidad_control(output wire s_inc , s_inm, we3, output wire [2:0] Op, output wire wez, input wire[5 : 0]opcode, input wire reloj, reset, 1);
+module unidad_control(output reg s_inc , s_inm, we3, output reg [2:0] Op, output reg wez, input wire[5 : 0]opcode, input wire reloj, reset, 1);
   always @(*)
   begin
-    if (reloj == 1'b1 && reset == 1'b0) {
+    if (reloj == 1'b1 && reset == 1'b0) begin
       casex (opcode)
           6'b0000??:
-          assign s_inc = 1
-          assign s_inm = 1
-          assign Op = 000
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 1
+          Op <= 000
+          we3 <= 1
+          wez <= 1
           6'b0001??:
-          assign s_inc = 1
-          assign s_inm = 1
-          assign Op = 010
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 1
+          Op <= 010
+          we3 <= 1
+          wez <= 1
           6'b0010??:
-          assign s_inc = 1
-          assign s_inm = 1
-          assign Op = 011
-          assign we3 = 0
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 1
+          Op <= 011
+          we3 <= 0
+          wez <= 1
           6'b0011??:
-          assign s_inc = 1
-          assign s_inm = 1
-          assign Op = 110
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 1
+          Op <= 110
+          we3 <= 1
+          wez <= 1
           6'b0100??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 000
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 000
+          we3 <= 1
+          wez <= 1
           6'b0101??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 001
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 001
+          we3 <= 1
+          wez <= 1
           6'b0110??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 010
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 010
+          we3 <= 1
+          wez <= 1
           6'b0111??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 011
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 011
+          we3 <= 1
+          wez <= 1
           6'b1000??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 100
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 100
+          we3 <= 1
+          wez <= 1
           6'b1001??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 101
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 101
+          we3 <= 1
+          wez <= 1
           6'b1010??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 110
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 110
+          we3 <= 1
+          wez <= 1
           6'b1011??:
-          assign s_inc = 1
-          assign s_inm = 0
-          assign Op = 111
-          assign we3 = 1
-          assign wez = 1
+          s_inc <= 1
+          s_inm <= 0
+          Op <= 111
+          we3 <= 1
+          wez <= 1
           6'b1111??:
-          assign s_inc = 0
-          assign s_inm = 0
-          assign Op = 000
-          assign we3 = 0
-          assign wez = 0
+          s_inc <= 0
+          s_inm <= 0
+          Op <= 000
+          we3 <= 0
+          wez <= 0
       endcase      
-    }
-    if (reloj == 1'b0 && reset == 1'b1) {
-      
-    }
+    end
+    if (reloj == 1'b0 && reset == 1'b1) begin
+      s_inc <= 0
+      s_inm <= 0
+      Op <= 000
+      we3 <= 0
+      wez <= 0
+    end
   end
 endmodule
